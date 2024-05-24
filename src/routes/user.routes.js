929a15cc741261172489578127ef97b589a970fb
Tejas Kumar <tejas22052004@gmail.com>
2024-05-24 15:57:13 +0530

    Added controller for user watch history and added all routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword,
+         getCurrentUser,
+         getUserChannelProfile, 
+         getUserWatchHistory, 
+         loginUser, 
+         logoutUser, 
+         refreshAccessToken, 
+         registerUser, 
+         updateUserAvatar, 
+         updateUserCoverImage, 
+         updateUserDetails
+         } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from './../middlewares/auth.middleware.js';
 
@@ -25,7 +36,7 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser)
 
-//Secred Routes
+//Secured Routes
 router.route("/logout").post(verifyJWT,logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken) //no need of verify JWT here
@@ -33,5 +44,12 @@ router.route("/refresh-token").post(refreshAccessToken) //no need of verify JWT 
 // router.route("/login").post(login)
 //remember this is post not get
 
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateUserDetails)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getUserWatchHistory)
 
 export default router
